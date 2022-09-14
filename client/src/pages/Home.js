84faa1526f8ce2b6051307295825823a98e1c91b
jscbridge <jorge.sanchez@thebridgeschool.es>
2022-09-14 17:54:49 +0200

    login/registro estilos

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,23 +1,28 @@
-
 import { Link } from 'react-router-dom'
+import './css/home.css'
+import portada from './img/taskImg.jpg'
 
 function Home() {
-    return (
-      <div >
-       <div className='Bienvenida'>
-        <p className="text">Bienvenido a Organizador de Tareas</p>
-        
+  return (
+    <div className="home">
+      <div className="Bienvenida">
+        <img className="img-port" src={portada}></img>
+        <div>
+          <p className="text">Bienvenido a Organizador de Tareas</p>
 
-        <p className="text2">Organízate, planifica y guarda las tareas</p>
+          <p className="text2">Organízate, planifica y guarda las tareas</p>
+        </div>
+      </div>
+      <div className="buttons">
+        <button className="login">
+          <Link to={'/login'}> Iniciar sesión</Link>
+        </button>
+        <button className="register">
+          <Link to={'/register'}>Crear una cuenta</Link>
+        </button>
+      </div>
+    </div>
+  )
+}
 
-      </div>
-      <div className='buttons'>
-        <button className='login'><Link to={"/login"} > Iniciar sesión</Link></button>
-        <button className='register'><Link to={"/register"} >Crear una cuenta</Link></button>
-      </div>
-      </div>
-    );
-  }
-  
-  export default Home;
-  
+export default Home
--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,11 +1,48 @@
+import React, {useState } from 'react'
 
 function Login() {
-    return (
-      <div >
-        Login
-      </div>
-    );
+  const [email, setEmail] = useState('')
+  const [pass, setPass] = useState('')
+
+
+  const loguear = () => {
+    let giveUser = {
+      email,
+      pass,
+    }
+
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ giveUser }),
+    }
+
+    fetch('loguear', requestOptions)
+      .then((res) => res.json())
+      .then((res) => {})
+
+
   }
-  
-  export default Login;
-  
+
+  return (
+    <div>
+      <div className="form-register">
+        <label>Email</label>
+        <input
+          type="email"
+          placeholder="tareas@gmail.com"
+          onChange={(e) => setEmail(e.target.value)}
+        ></input>
+        <label>Password</label>
+        <input
+          type="password"
+          onChange={(e) => setPass(e.target.value)}
+        ></input>
+       
+        <input type="button" value="Loguear" onClick={() => loguear()} />
+      </div>
+    </div>
+  )
+}
+
+export default Login
